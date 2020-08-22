ae32b011c64b612d8f25e3952b2070e09a6d08b6
Holger Kurtz <kulturdata@gmail.com>
2020-08-22 11:06:59 +0200

    cleaning up the code

--- twitter-get.py
+++ twitter-get.py
@@ -37,27 +37,22 @@ def get_followers():
         user_list.append(follower.screen_name)
         follower_list.append(int(follower.followers_count))
         
-
     _dict['date'] = date_list
     _dict['user'] = user_list
     _dict['follower'] = follower_list
 
     # Dict --> DataFrame
     data = pd.DataFrame.from_dict(_dict)
-    # print ("===vor abspeichern:===\n", data)
 
     # DataFrame --> CSV File
     data.to_csv('db.csv', mode='a', header=False, index=False)
-
 
 def get_2_most_recent_data():
     data = pd.read_csv('db.csv')
     data['date'] = pd.to_datetime(data['date']) #dtype manipulation to datetime
     data['follower'] = pd.to_numeric(data['follower']) #dtype manipulation to integer
-    # print ("===aus csv===\n", data, '\n', "===Type===", data.dtypes)
     
     date_group_by = data.groupby(data['date']).sum()
-    # print("===grouped\n===", date_group_by)
 
     recent_date = data['date'].max()
     recent_followers = data.loc[data['date'] == recent_date ] # newest rows
@@ -66,10 +61,6 @@ def get_2_most_recent_data():
     second_date = data['date'].drop_duplicates().nlargest(2).iloc[-1]
     second_recent_followers = data.loc[data['date'] == second_date ] # second newest row
     second_recent_followers.reset_index(drop=True, inplace=True)
-
-
-    # combination = [recent_followers, second_recent_followers ]
-    # recent_df = pd.concat(combination)
     return recent_followers, second_recent_followers
 
 def calculate_growth():
@@ -78,21 +69,17 @@ def calculate_growth():
     new_dict = {}
     for user in recent_followers['user']:
         follower_count = recent_followers.loc[recent_followers['user'] == user].iloc[0]['follower']
-
         follower_count_2 = second_recent_followers.loc[second_recent_followers['user'] == user].iloc[0]['follower']
+        
         diff = follower_count - follower_count_2
-
         new_dict[user] = diff
     return new_dict
 
 def tweet():
     new_dict = calculate_growth()
-    ordered_dict = {k: v for k, v in sorted(new_dict.items(), key=lambda item: item[1], reverse=True)}
-    # print ("===ordered_List===\n", ordered_dict)
+    ordered_dict = {k: v for k, v in sorted(new_dict.items(), key=lambda item: item[1], reverse=True)} #not mine but works fine
     top_user = list(ordered_dict.keys())
     top_growth = list(ordered_dict.values())
-
-    # print ("top user\n", top_user,"\n Top growth", top_growth )
  
     text = f'''
     Top3 meiner Follower mit größtem Wachstum 🚀 (7 Tage):
@@ -101,11 +88,8 @@ def tweet():
     Platz 3: @{top_user[2]} + {top_growth[2]} Follower
     Gratulation! 🎊
     '''
-
-
     print (text, "Zeichenanzahl:", len(text))
-
-
+    #api.update_status(text)
 
 if __name__ == "__main__":
     # get_followers()
